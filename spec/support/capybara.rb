26e9a6c4ac3a271bd892a2f52e459011a760752a
xjb.laptop <xjbwks+laptop@gmail.com>
2021-09-24 21:18:28 +0900

    Capybara をセットアップ

---
+++ spec/support/capybara.rb
@@ -0,0 +1,36 @@
+require 'selenium-webdriver'
+
+Capybara.configure do |config|
+end
+
+Capybara.register_driver :remote_chrome do |app|
+  args = [
+    'no-sandbox',
+    'headless',
+    'disable-gpu',
+    'window-size=1680,1050'
+  ]
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+    'goog:chromeOptions' => {
+      'args' => args
+    }
+  )
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :remote,
+    url: ENV['SELENIUM_HUB_URL'],
+    capabilities: caps
+  )
+end
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+    Capybara.app_host = "http://#{Capybara.server_host}"
+    driven_by :remote_chrome
+  end
+end
